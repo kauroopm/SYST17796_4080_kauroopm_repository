1ecb19e221d5df4a091b0dd00158edba568235da
kauroopm <59941731+kauroopm@users.noreply.github.com>
2020-01-29 18:49:48 -0500

    Update ArrayObjects.java

--- src/ArrayObjects/ArrayObjects.java
+++ src/ArrayObjects/ArrayObjects.java
@@ -12,9 +12,13 @@ public class ArrayObjects {
         this.Names = Names;
     }
     public static void main(String[] args) {
-        String myArray[] = new String[2];
+        String myArray[] = new String[6];
         myArray[0] = "Dhoom";
         myArray[1] = "Dhoom2";
+        myArray[2] = "Dhoom3";
+        myArray[3] = "Dhoom4";
+        myArray[4] = "Dhoom5";
+        myArray[5] = "Dhoom6";
         
         for (String myArray1 : myArray) {
             System.out.println(myArray1);
